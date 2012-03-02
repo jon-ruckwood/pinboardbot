4ec0c869a0638497a89c3e5b72bcb5af2a162352
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-03-02 17:28:13 +0000

    Settled on using twitter4j, externalised api keys

---
+++ src/main/scala/TwitterClient.scala
@@ -0,0 +1,22 @@
+import org.slf4j.LoggerFactory
+import twitter4j.{TwitterFactory, Twitter}
+import twitter4j.auth.AccessToken
+import org.streum.configrity._
+
+object TwitterClient extends App {
+	val log = LoggerFactory.getLogger(getClass)
+
+	val config = Configuration.load(System.getProperty("pinboardbotDeploymentConfig"))
+
+	val twitter = new TwitterFactory().getInstance()
+	twitter.setOAuthConsumer(config[String]("oauthConsumerKey"), 
+		config[String]("oauthConsumerSecret")) 
+
+	// TODO: Externalise tokens
+	val token = new AccessToken(config[String]("accessToken"),
+		config[String]("accessTokenSecret")) 
+	twitter.setOAuthAccessToken(token)
+
+
+	log.debug("Tweets: {}", twitter.getUserTimeline())
+}
