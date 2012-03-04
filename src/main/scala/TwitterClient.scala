4d9cf750990b8be29a7fa4786b8152098e666f7b
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-03-04 17:45:30 +0000

    Converted TwitterClient to use streaming api instead, implemented basic status listener

--- src/main/scala/TwitterClient.scala
+++ src/main/scala/TwitterClient.scala
@@ -1,5 +1,5 @@
 import org.slf4j.LoggerFactory
-import twitter4j.{TwitterFactory, Twitter}
+import twitter4j._
 import twitter4j.auth.AccessToken
 import org.streum.configrity._
 
@@ -7,15 +7,38 @@ object TwitterClient extends App {
 	val log = LoggerFactory.getLogger(getClass)
 
 	val config = Configuration.load(System.getProperty("pinboardbotDeploymentConfig"))
+	val oauthConsumerKey = config[String]("oauthConsumerKey")
+	val oauthConsumerSecret = config[String]("oauthConsumerSecret")
+	val accessToken = config[String]("accessToken")
+	val accessTokenSecret = config[String]("accessTokenSecret")
 
-	val twitter = new TwitterFactory().getInstance()
-	twitter.setOAuthConsumer(config[String]("oauthConsumerKey"), 
-		config[String]("oauthConsumerSecret")) 
+	val twitterStream = new TwitterStreamFactory().getInstance()
+	twitterStream.setOAuthConsumer(oauthConsumerKey, oauthConsumerSecret)
 
-	val token = new AccessToken(config[String]("accessToken"),
-		config[String]("accessTokenSecret")) 
-	twitter.setOAuthAccessToken(token)
+	val token = new AccessToken(accessToken, accessTokenSecret) 
+	twitterStream.setOAuthAccessToken(token)
 
+	twitterStream.addListener(new StatusListener() {
+		def onStatus(status: Status) {
+			log.info("{} - '{}'", status.getUser().getName(), status.getText())
+		}
 
-	log.debug("Tweets: {}", twitter.getUserTimeline())
+		def onDeletionNotice(notice: StatusDeletionNotice) {
+			// 
+		}
+
+		def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {
+			// 
+		}
+
+		def onScrubGeo(userId: Long, upToStatusId: Long) {
+			//
+		}
+
+		def onException(ex: Exception) {
+			log.error("Exception: {}", ex)
+		}
+	})
+
+	twitterStream.sample()
 }
