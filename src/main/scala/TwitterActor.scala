0a04c90c2e3b34c6d44d5efaedb5ef6c35000d10
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-03-15 18:21:01 +0000

    Renamed client to actor

---
+++ src/main/scala/TwitterActor.scala
@@ -0,0 +1,31 @@
+import org.slf4j.LoggerFactory
+import twitter4j._
+import twitter4j.auth.AccessToken
+import scala.collection.JavaConversions._
+import com.typesafe.config.ConfigFactory
+
+object TwitterClient extends App {
+	val log = LoggerFactory.getLogger(getClass)
+
+	val conf = ConfigFactory.load()
+	val oauthConsumerKey = conf.getString("oauthConsumerKey")
+	val oauthConsumerSecret = conf.getString("oauthConsumerSecret")
+	val userAccessToken = conf.getString("accessToken")
+	val userAccessTokenSecret = conf.getString("accessTokenSecret")	
+
+	val twitter = new TwitterFactory().getInstance()
+	twitter.setOAuthConsumer(oauthConsumerKey, oauthConsumerSecret)
+
+	val accessToken = new AccessToken(userAccessToken, userAccessTokenSecret)
+	twitter.setOAuthAccessToken(accessToken)
+
+	val mentions = twitter.getMentions()
+
+	mentions.foreach { status =>
+		log.info("Mention -> {}", 
+			Array(status.getId(), status.getUser().getScreenName(), 
+				status.getURLEntities(), status.getHashtagEntities())
+		)
+	}
+	
+}
