6bb47e8dac60bee57752960fcae6ef695b046571
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-03-23 09:49:06 +0000

    Began adding TwitterActor tests

--- src/test/scala/TwitterActorSpec.scala
+++ src/test/scala/TwitterActorSpec.scala
@@ -2,5 +2,8 @@ import org.scalatest.Spec
 import org.scalatest.GivenWhenThen
 
 class TwitterActorSpec extends Spec with GivenWhenThen {
-	// TODO
+	
+	describe("TwitterActor") {
+		it("Should get Twitter mentions when a FetchTweets message is received") (pending)
+	}
 }
