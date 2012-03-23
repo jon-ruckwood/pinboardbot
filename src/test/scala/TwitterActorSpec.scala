e688c4f00bd76ffc12338df7a40d099b2d86baa4
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-03-23 14:11:34 +0000

    Added more test cases

--- src/test/scala/TwitterActorSpec.scala
+++ src/test/scala/TwitterActorSpec.scala
@@ -4,6 +4,10 @@ import org.scalatest.GivenWhenThen
 class TwitterActorSpec extends Spec with GivenWhenThen {
 	
 	describe("TwitterActor") {
-		it("Should get Twitter mentions when a FetchTweets message is received") (pending)
+		it("Should get mentions when a FetchTweets message is received") (pending)
+		it("Should only get new mentions since last time it ran") (pending)
+		it("Should only get new mentions since last time it ran after restart") (pending)
+		// TODO: Rename tweets -> mentions?
+		it("Should send each mention back to the sender") (pending)
 	}
 }
