dba71d0efeefe021e994d0166bf0510c7bba6889
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-04-16 13:57:21 +0100

    Started adding tests instead of diving in

---
+++ src/test/scala/Twitter4JTwitterClientSpec.scala
@@ -0,0 +1,10 @@
+import org.scalatest.Spec
+import org.scalatest.GivenWhenThen
+
+class Twitter4JTwitterClientSpec extends Spec with GivenWhenThen {
+	
+	describe("The client") {
+		it("Should return an empty list when there are no mentions") (pending)
+		it("Should return mentions since the specified Tweet Id") (pending)
+	}
+}
