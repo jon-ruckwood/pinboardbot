b248a5dee248c7301a8bb2c550a3bc34b3891846
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-04-23 09:20:24 +0100

    Sketched out next Twitter4JClient spec, to be implemented.

--- src/test/scala/Twitter4JTwitterClientSpec.scala
+++ src/test/scala/Twitter4JTwitterClientSpec.scala
@@ -1,5 +1,4 @@
-import org.scalatest.FunSpec
-import org.scalatest.GivenWhenThen
+import org.scalatest.{FunSpec, GivenWhenThen, BeforeAndAfterEach}
 import org.scalatest.mock.MockitoSugar
 import org.mockito.BDDMockito
 import org.mockito.Matchers._
@@ -10,18 +9,22 @@ import twitter.Twitter4JTwitterClient
 import Prototype._
 import Values._
 
-class Twitter4JTwitterClientSpec extends FunSpec with GivenWhenThen with MockitoSugar {
+class Twitter4JTwitterClientSpec extends FunSpec with GivenWhenThen with BeforeAndAfterEach with MockitoSugar {
 	
+	var twitter: Twitter = _
+	var responseList: ResponseList[Status] = _
+
+	override def beforeEach() {
+		twitter = mock[Twitter]
+		responseList = mock[ResponseList[Status]]
+	}
+
 	describe("The client") {
 		it("Should return an empty list when there are no mentions") {
 			given("the underlying Twitter client returns no mentions")
 
-			val twitter = mock[Twitter]
-			val responseList = mock[ResponseList[Status]]
-
-			BDDMockito.given(twitter.getMentions(any(classOf[Paging]))).willReturn(responseList)
-
 			BDDMockito.given(responseList.iterator()).willReturn(emptyJavaIterator[Status]())
+			BDDMockito.given(twitter.getMentions(any(classOf[Paging]))).willReturn(responseList)
 
 			val client = new Twitter4JTwitterClient(twitter)
 
@@ -32,6 +35,16 @@ class Twitter4JTwitterClientSpec extends FunSpec with GivenWhenThen with Mockito
 			then("the returned list must be empty")
 
 			assert(mentions.isEmpty)
+		}
+
+		it("Should return single mention") {
+			given("the underlying Twitter client returns a mention")
+
+
+			when("mentions are fetched")
+
+
+			then("the returned list contains the single mention")
 		}
 
 		it("Should return mentions since the specified Tweet Id") (pending)
