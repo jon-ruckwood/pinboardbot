566450af178691e5ae5b1ac3f4da7a7118803ed5
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-05-02 17:46:43 +0100

    Beginning to look at TwitterActor tests, adding extra cases. Removed todos from actor that I don't understand.

--- src/main/scala/net/selfdotlearn/pinboardbot/TwitterActor.scala
+++ src/main/scala/net/selfdotlearn/pinboardbot/TwitterActor.scala
@@ -15,10 +15,8 @@ class TwitterActor(val twitterClient: TwitterClient) extends Actor {
 		case FetchTweets => 
 			log.info("Fetching Tweets from Twitter. Tweet-tweet!")
 
-			// TODO: Default isn't working for some reason...?
 			twitterClient.fetchMentions(1).foreach { tweet => 
 				// TODO: Cross terminology is confusing, have something domain specific?
-				// TODO: Handle else case
 				sender ! Mention(tweet.id, tweet.url.getOrElse(""), tweet.tags)	
 			}
 		case _ => log.info("Received unknown message")
--- src/test/scala/net/selfdotlearn/pinboardbot/TwitterActorSpec.scala
+++ src/test/scala/net/selfdotlearn/pinboardbot/TwitterActorSpec.scala
@@ -1,14 +1,14 @@
 package net.selfdotlearn.pinboardbot
 
-import org.scalatest.FunSpec
-import org.scalatest.GivenWhenThen
+import net.selfdotlearn.pinboardbot.test.Specs.UnitTestSpec
 
-class TwitterActorSpec extends FunSpec with GivenWhenThen {
+class TwitterActorSpec extends UnitTestSpec {
 	
 	describe("TwitterActor") {
 		it("Should get mentions when a FetchTweets message is received") (pending)
+		it("Should send each mention back to the sender") (pending)
+		it("Should drop mentions without a url present") (pending)
 		it("Should only get new mentions since last time it ran") (pending)
 		it("Should only get new mentions since last time it ran after restart") (pending)
-		it("Should send each mention back to the sender") (pending)
 	}
 }
