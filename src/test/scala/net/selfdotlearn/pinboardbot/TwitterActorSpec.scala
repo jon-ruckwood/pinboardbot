4bc2484bbed12f7ae8f4397906f0eda2a5802f60
Jonathan Ruckwood <jonathan.ruckwood@gmail.com>
2012-08-25 11:21:06 +0100

    The ImplicitSender mixin was missing on the spec, this is why it failed

--- src/test/scala/net/selfdotlearn/pinboardbot/TwitterActorSpec.scala
+++ src/test/scala/net/selfdotlearn/pinboardbot/TwitterActorSpec.scala
@@ -5,8 +5,7 @@ import org.scalatest.GivenWhenThen
 import org.scalatest.BeforeAndAfterAll
 import org.mockito.{BDDMockito, Mockito}
 
-import akka.testkit.TestActorRef
-import akka.testkit.TestKit
+import akka.testkit.{TestKit, TestActorRef, ImplicitSender}
 import akka.actor.{Actor, ActorSystem, Props}
 
 import net.selfdotlearn.pinboardbot.test.Values._
@@ -15,7 +14,7 @@ import net.selfdotlearn.pinboardbot.test.Specs.UnitTestSpec
 import net.selfdotlearn.pinboardbot.twitter.{TwitterClient, Tweet}
 import net.selfdotlearn.pinboardbot.Messages.{FetchMentions, Mention}
 
-class TwitterActorSpec extends TestKit(ActorSystem()) with UnitTestSpec with BeforeAndAfterAll {
+class TwitterActorSpec extends TestKit(ActorSystem()) with ImplicitSender with UnitTestSpec with BeforeAndAfterAll {
 	
 
 	describe("TwitterActor") {
@@ -28,7 +27,7 @@ class TwitterActorSpec extends TestKit(ActorSystem()) with UnitTestSpec with Bef
 				new Tweet(id = 2, url = Some("http://www.twitter.com"), tags = Set("twitter", "social")))
 			)
 
-			val twitterActorRef = TestActorRef(new TwitterActor(twitterClient))
+			val twitterActorRef = system.actorOf(Props(new TwitterActor(twitterClient)))
 
 			when("A FetchTweets message is recieved")
 
@@ -36,12 +35,10 @@ class TwitterActorSpec extends TestKit(ActorSystem()) with UnitTestSpec with Bef
 
 			then("Mentions are sent to the sender")
 
-			// TODO: Determine why this expectation does not hold
-			/*
 			expectMsgAllOf(
 				Mention(id = 1, url = "http://www.google.com", tags = Set("google", "search")),
 				Mention(id = 2, url = "http://www.twitter.com", tags = Set("twitter", "social"))
-			)*/
+			)
 		}
 
 		it("Should not send any mentions when there are none") (pending)
